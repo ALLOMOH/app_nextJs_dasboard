2b268b5bb28e8ea2ef9a7b0badef6f9635bc7837
ALLOMOH <allomohangealex@gmail.com>
2025-04-21 23:44:24 +0000

    first commit nextjs

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <p> Custorm Page</p>
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import {Inter}  from 'next/font/google'
+import { Lusitana } from 'next/font/google';
+const lusitana = Lusitana({subsets:['latin'], weight: ['400']});
+const inter = Inter({subsets:['latin']});
+
+export {lusitana,inter};
